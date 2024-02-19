ed0f8fdbc968ee32dfed90c75ac64be88bd933c5
Saufi <msaufi2325@gmail.com>
2024-02-19 12:55:05 +0900

    Add pathToTemplates variable

--- internal/render/render.go
+++ internal/render/render.go
@@ -16,6 +16,7 @@ import (
 var functions = template.FuncMap{}
 
 var app *config.AppConfig
+var pathToTemplates = "./templates"
 
 // NewTemplates sets the config for the template package
 func NewTemplates(a *config.AppConfig) {
