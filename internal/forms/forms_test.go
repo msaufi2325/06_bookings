7a65e852cb8db1240d7e8b46d24dd8f3f16940e1
Saufi <msaufi2325@gmail.com>
2024-02-21 19:57:47 +0900

    Add test for forms

---
+++ internal/forms/forms_test.go
@@ -0,0 +1,44 @@
+package forms
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestForm_Valid(t *testing.T) {
+	r := httptest.NewRequest("POST", "/some-url", nil)
+	form := New(r.PostForm)
+
+	isValid := form.Valid()
+	if !isValid {
+		t.Error("got invalid when should have been valid")
+	}
+}
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest("POST", "/some-url", nil)
+	form := New(r.PostForm)
+
+	form.Required("a", "b", "c")
+	isValid := form.Valid()
+	if isValid {
+		t.Error("got valid when should have been invalid")
+	}
+
+	postData := url.Values{}
+	postData.Add("a", "a")
+	postData.Add("b", "a")
+	postData.Add("c", "a")
+
+	r, _ = http.NewRequest("POST", "/some-url", nil)
+	r.PostForm = postData
+	form = New(r.PostForm)
+	form.Required("a", "b", "c")
+
+	isValid = form.Valid()
+	if !isValid {
+		t.Error("got invalid when should have been valid")
+	}
+}
