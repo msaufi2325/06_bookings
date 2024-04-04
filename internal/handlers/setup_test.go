a7d4cf8ee08eae54f0a27ad202e460fc4f2efc93
Saufi <msaufi2325@gmail.com>
2024-04-05 03:02:42 +0900

    Add mail listener in setup_test.go

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -46,6 +46,12 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	defer close(mailChan)
+
+	listenForMail()
+
 	tc, err := CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
@@ -59,6 +65,14 @@ func TestMain(m *testing.M) {
 	render.NewRenderer(&app)
 
 	os.Exit(m.Run())
+}
+
+func listenForMail() {
+	go func() {
+		for {
+			<-app.MailChan
+		}
+	}()
 }
 
 func getRoutes() http.Handler {
