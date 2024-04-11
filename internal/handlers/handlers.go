928b97505686aa11eb612012f45d944ce12bd2dd
Saufi <msaufi2325@gmail.com>
2024-04-12 01:47:42 +0900

    Add handler function for processing reservations in admin dashboard

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/msaufi2325/06_bookings/internal/config"
 	"github.com/msaufi2325/06_bookings/internal/driver"
 	"github.com/msaufi2325/06_bookings/internal/forms"
@@ -620,4 +621,13 @@ func (m *Repository) AdminPostShowReservation(w http.ResponseWriter, r *http.Req
 // AdminReservationsCalendar shows the reservation calendar on the admin dashboard
 func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{})
+}
+
+// AdminProcessReservation marks a reservation as processed
+func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+	src := chi.URLParam(r, "src")
+	_ = m.DB.UpdateProcessedForReservation(id, 1)
+	m.App.Session.Put(r.Context(), "flash", "Reservation marked as processed")
+	http.Redirect(w, r, "/admin/reservations-"+src, http.StatusSeeOther)
 }
